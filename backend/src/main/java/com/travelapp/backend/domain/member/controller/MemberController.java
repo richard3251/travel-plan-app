b575b8024fda2be39b83de0f9181e2b4535f3ead
richard3251 <paladog31@naver.com>
2025-05-05 22:48:06 +0900

    feat: MemberController 작성완료.

--- backend/src/main/java/com/travelapp/backend/domain/member/controller/MemberController.java
+++ backend/src/main/java/com/travelapp/backend/domain/member/controller/MemberController.java
@@ -1,6 +1,13 @@
 package com.travelapp.backend.domain.member.controller;
 
+import com.travelapp.backend.domain.member.dto.request.MemberLoginRequest;
+import com.travelapp.backend.domain.member.dto.request.MemberSignUpRequest;
+import com.travelapp.backend.domain.member.dto.response.MemberResponse;
+import com.travelapp.backend.domain.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +15,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/members")
 public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<MemberResponse> signup(
+        @RequestBody MemberSignUpRequest request
+    ) {
+        MemberResponse response = memberService.signUp(request);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<MemberResponse> login(
+        @RequestBody MemberLoginRequest request
+    ) {
+        MemberResponse response = memberService.login(request);
+
+        return ResponseEntity.ok(response);
+    }
 
 }
